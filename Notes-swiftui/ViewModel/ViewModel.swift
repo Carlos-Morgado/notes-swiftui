a4ddfaa24d5218b27818f137ef982b7d419ef581
Carlos Morgado <cmorgadoalarcon@gmail.com>
2024-03-20 17:26:26 +0100

    * Feature/Integration-test: Use cases

--- Notes-swiftui/ViewModel/ViewModel.swift
+++ Notes-swiftui/ViewModel/ViewModel.swift
@@ -12,13 +12,34 @@ import Observation
 class ViewModel {
     var notes: [NoteModel]
     
-    init(notes: [NoteModel] = []) {
+    var createNoteUseCase: CreateNoteUseCase
+    var fetchAllNotesUseCase: FetchAllNotesUseCase
+    
+    init(notes: [NoteModel] = [], 
+         createNoteUseCase: CreateNoteUseCase = CreateNoteUseCase(),
+         fetchAllNotesUseCase: FetchAllNotesUseCase = FetchAllNotesUseCase()) {
         self.notes = notes
+        self.createNoteUseCase = createNoteUseCase
+        self.fetchAllNotesUseCase = fetchAllNotesUseCase
+        fetchAllNotes()
     }
     
     func createNoteWith(title: String, text: String) {
-        let note: NoteModel = .init(title: title, text: text, createdAt: .now)
-        notes.append(note)
+        do {
+            try createNoteUseCase.createNoteWith(title: title, text: text)
+            fetchAllNotes()
+        } catch {
+            print("Error \(error.localizedDescription)")
+            
+        }
+    }
+    
+    func fetchAllNotes() {
+        do {
+            notes = try fetchAllNotesUseCase.fetchAll()
+        } catch {
+            print("Error \(error.localizedDescription)")
+        }
     }
     
     func updateNoteWith(identifier: UUID, newTitle: String, newText: String?) {
@@ -27,7 +48,7 @@ class ViewModel {
             notes[index] = updateNote
         }
     }
-    
+
     func removeNoteWith(identifier: UUID) {
         notes.removeAll(where: { $0.identifier == identifier })
     }
